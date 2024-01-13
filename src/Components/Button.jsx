3a15db71e97e9037350bdf9e951b10add92367f5
abhishekghanekar13 <142926791+abhishekghanekar13@users.noreply.github.com>
2024-01-13 17:07:47 +0530

    counter

---
+++ src/Components/Button.jsx
@@ -0,0 +1,61 @@
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+
+let Button = () => {
+  let numbercounter = useRef();
+
+  let dispach = useDispatch();
+
+  let handalIncrement = () => {
+    dispach({ type: "Increment" });
+  };
+
+  let handalDecrement = () => {
+    dispach({ type: "Decrement" });
+  };
+
+  let handal = () => {
+    dispach({
+      type: "Add",
+      payload: {
+        num: numbercounter.current.value,
+      },
+    });
+  };
+  return (
+    <>
+      <div className="col-lg-6 mx-auto">
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={handalIncrement}
+          >
+            +1
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={handalDecrement}
+          >
+            -1
+          </button>
+        </div>
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <input
+            type="text"
+            placeholder="Enter the number"
+            className="input"
+            ref={numbercounter}
+          />
+
+          <button type="button" className="btn btn-primary" onClick={handal}>
+            add
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Button;
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import {createStore} from "redux";
+let INISTAL_VALUE={
+  counter:0,
+}
+let counterstore=(store=INISTAL_VALUE,action)=>{
+    if(action.type==="Increment"){
+     return {counter:store.counter+1}
+    }
+    else if(action.type==="Decrement"){
+    return {counter:store.counter-1}
+    }
+    else if(action.type==="Add"){
+      return{counter: store.counter + Number(action.payload.num)}
+    }
+    return store;
+}
+let reducestore=createStore(counterstore);
+export default reducestore;
